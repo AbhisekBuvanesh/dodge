22f9f6f73cf62558173c67a403244d12fead3752
Abhisek <65477580+AbhisekBuvanesh@users.noreply.github.com>
2020-05-17 20:54:06 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ PLAYER_WIDTH = 35
 PLAYER_HEIGHT = 35
 #This is the size of the player
 BACKGROUND_COLOR = (0,0,0)
-#This is the color that we use for the backgroung when we start teh game.
+#This is the color that we use for the backgroung when we start the game.
 PLAYER_SPEED = 10
 #This is how fast the player travels on the screen
 WANDER_WIDTH = 45
@@ -13,10 +13,10 @@ WANDER_HEIGHT = 45
 #This is the size for the enemy Wander
 MIN_WANDER_SPEED = 1
 MAX_WANDER_SPEED = 5
-#This is the max and min speed the wander can achive
+#This is the max and min speed the Wander can achive
 GOAL_WIDTH = 60
 GOAL_HEIGHT = 60
-#This is the size of the goal the player has to go to.
+#This is the size of the goal the player has to go to
 HOMING_WIDTH = 25
 HOMING_HEIGHT = 25
 #This is the size for the homing enemy
@@ -28,4 +28,4 @@ SNAKE_HEIGHT = 60
 WANDER_CHANCE = .5
 SNAKE_CHANCE = .75
 HOMING_CHANCE = 1
-#These are the chances that each obstacle will appear on the scree.
+#These are the chances that each obstacle will appear on the screen
